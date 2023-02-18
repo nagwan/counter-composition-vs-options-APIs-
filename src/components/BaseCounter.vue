2af55590b052d8653ccd8bc7faccc13530473efe
nagwan <nagwan@arabyads.com>
2023-02-18 07:47:39 +0200

    BaseCounter with options APIs

---
+++ src/components/BaseCounter.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    components: {},
+    props: {},
+    data() {
+        return {
+            count: 0,
+        };
+    },
+    computed: {},
+    methods: {
+        increment() {
+            this.count++;
+        },
+
+        decrement() {
+            this.count--;
+        },
+    },
+};
+</script>
+<template>
+    <div>
+        <button @click="decrement">-</button>
+        Count is {{ count }}
+        <button @click="increment">+</button>
+    </div>
+</template>
+<style scoped lang='sass'></style>
